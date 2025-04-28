34acbafd483e20263271249cd8623ca5b560b405
Henry Thuler <henrythuler2003@outlook.com>
2025-04-28 20:24:39 -0300

    feat: common header implemented

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <TheHeader />
   <RouterView />
 </template>
 
 <script setup>
   import { RouterView } from 'vue-router'
+  import TheHeader from '@/components/layout/TheHeader.vue'
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import { createPinia } from 'pinia'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
