905a18c26e16b5ccb40bd99e04cdb4cf479f879d
Henry Thuler <henrythuler2003@outlook.com>
2025-04-28 19:18:12 -0300

    feat: basic router and Translators/Documents views implemented

---
+++ src/views/DocumentsView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-white">Documents</div>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/TranslatorsView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-white">Translators</div>
+</template>
+
+<script setup> 
+</script>
